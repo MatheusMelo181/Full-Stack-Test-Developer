b48c1c2ccac2fd52f67d1346d7cf36cc5fd1e322
Matheus Rocha <mather35@braskem.com>
2023-07-24 11:00:28 -0300

    sass e redux

--- Front/redux-front/src/main.tsx
+++ Front/redux-front/src/main.tsx
@@ -1,15 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { Provider } from 'react-redux';
-
-import movie from "./redux/movies";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store = {movie}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
